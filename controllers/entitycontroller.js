a7fcc809808dc5495032ee2ebc4246ac598b6b48
Marco Gregori <marco.gregori74@gmail.com>
2022-10-05 16:25:46 -0300

    Actualizacion

--- controllers/entitycontroller.js
+++ controllers/entitycontroller.js
@@ -18,20 +18,37 @@ const getHeroes = (req = request ,res) => {
     const querys = req.query
     const querysKeys = Object.keys(querys);
     let searchParams = "";
+    let invalid = false;
     if (querysKeys.length != 0){
+    
+    // Intento de validacion de parametros sin usar express-validator,
+    // ignora querys vacias
+
+
+    // Nota: Para buscar por serie y comics en necesario tener la id del personaje
+
     querysKeys.forEach(element => { 
-        if (!(element === "name" || element === "comics" || element === "series")){res.send({400 : "BAD request"}) /* res.status(500);*/ }
-        else searchParams += (element +"="+querys[element] + "&")
+        if ((element === "name" || element === "comics" || element === "series")){(querys[element].trim().length === 0) ? invalid = true : searchParams += (element +"="+querys[element] + "&")}
+        else { invalid = true }
     });
     }
 
+    if (invalid){/*res.status(403),*/res.send({ERROR : 'Parametro vacio o invalido'})}
+
     axios.get(`https://gateway.marvel.com:443/v1/public/characters?limit=50&${searchParams}ts=${TS}&apikey=${PUBLIC_KEY}&hash=${HASH}`)
     .then(function (response) {
-      res.status(200),
-      res.send((response.data.data.results).map(function(obj){return [obj.name,obj.id]}))
+      res.send((response.data.data.results).map(function(obj){
+        return [
+        obj.name,
+        obj.id,
+        (obj.comics.items).map(function(obj){return obj.name}),
+        (obj.series.items).map(function(obj){return obj.name})
+    
+    ]}))
     })
     .catch(function (error) {
-        res.status(500), res.send({OOPS : "Algo salio mal"})
+        if(error.code == 'ERR_BAD_REQUEST'){res.status(404),res.send({404 : "No se encontro"})}
+        else{res.status(500),res.send({OOPS : "Algo salio mal"})}
     })
     
 }
@@ -43,11 +60,9 @@ const getHeroesById = (req = request ,res) => {
         res.status(200),res.send(response.data.data.results)
     })
     .catch(function (error) {
-        error.code == 'ERR_BAD_REQUEST' ? res.status(400)
-        : res.status(500)
-        res.send(error)
+        if(error.code == 'ERR_BAD_REQUEST'){res.status(404),res.send({404 : "No se encontro"})}
+        else{res.status(500),res.send({OOPS : "Algo salio mal"})}
     })
-    
 }
 
 module.exports = {
